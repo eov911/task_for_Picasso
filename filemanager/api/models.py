b1361181ca2b079afc492f68fdfcaba40941bca9
Оксана Ерегина <oksanaeregina@gmail.com>
2024-02-04 02:48:44 +0300

    Создана модель file. Миграции

--- filemanager/api/models.py
+++ filemanager/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    file = models.FileField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
+
